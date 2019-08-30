8eee67a2021558cb4aea1b6d79e01ea9eb923b79
Kirk <kirk.woll@gmail.com>
2019-08-29 18:54:16 -0700

    Fixes #3 Make sure repeat operator resets state upon failure

---
+++ PEG.Tests/RepeatTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PEG.SyntaxTree;
+
+namespace PEG.Tests
+{
+    [TestFixture]
+    public class RepeatTests
+    {
+        [Test]
+        public void RepeatResets()
+        {
+            var grammar = TestGrammar.Create();
+            var parser = new PegParser<TestData>(grammar, grammar.Root());
+            var input = "AAABBBBB";
+            var result = parser.Parse(input);
+            Assert.AreEqual(input, result.Items);
+        }
+
+        public class TestData
+        {
+            public string Items { get; set; }
+        }
+
+        public class TestGrammar : Grammar<TestGrammar>
+        {
+            public virtual Expression Root()
+            {
+                return Items();
+            }
+
+            public virtual Expression Items()
+            {
+                return -(A() | B());
+            }
+
+            public virtual Expression A()
+            {
+                return 'A'._().Repeat(4, 6);
+            }
+
+            public virtual Expression B()
+            {
+                return 'A'._() | 'B';
+            }
+        }
+    }
+}
--- PEG/SyntaxTree/Repeat.cs
+++ PEG/SyntaxTree/Repeat.cs
@@ -11,20 +11,28 @@ namespace PEG.SyntaxTree
 
         public override IEnumerable<OutputRecord> Execute(ParseEngine engine)
         {
-            IEnumerable<OutputRecord> result = NoResults;
+            var result = NoResults;
             IEnumerable<OutputRecord> current;
+
+            var mark = engine.Mark();
 
             int count;
             for (count = 0; !engine.IsFailure(current = Operand.Execute(engine)); count++)
             {
                 if (count > Max)
+                {
+                    engine.Reset(mark);
                     return null;
+                }
 
-                result = result.Concat(current);                
+                result = result.Concat(current);
             }
 
             if (count < Min)
+            {
+                engine.Reset(mark);
                 return null;
+            }
 
             return result;
         }
@@ -37,6 +45,6 @@ namespace PEG.SyntaxTree
         public override void Accept<T>(IExpressionVisitor<T> visitor, T context)
         {
             visitor.Visit(this, context);
-        }        
+        }
     }
 }
